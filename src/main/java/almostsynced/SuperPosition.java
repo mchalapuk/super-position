117354b0952f6e1d43f927ac0937e5cce5b69044
Maciej Chałapuk <maciej@chalapuk.pl>
2017-04-17 19:37:33 +0200

    ~ Renamed AsyncWheel to SuperPosition.

---
+++ src/main/java/almostsynced/SuperPosition.java
@@ -0,0 +1,173 @@
+package almostsynced;
+
+import javax.annotation.Nonnull;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+import static almostsynced.Preconditions.checkNotNull;
+import static almostsynced.Preconditions.checkState;
+
+/**
+ * @author Maciej Chałapuk &lt;maciej@chalapuk.pl&gt;
+ */
+public class SuperPosition<Space> {
+    public interface Reader<Space> {
+        boolean read(@Nonnull Consumer<Space> readTick);
+    }
+
+    public interface Mover<Space> {
+        void read(@Nonnull Consumer<Space> readTick);
+        void move(@Nonnull Consumer<Space> moveTick);
+    }
+
+    private final AtomicReference<Position<Space>> free = new AtomicReference<>(); // accessed by reader and mover
+
+    private Mover<Space> mover;
+    private Reader<Space> reader;
+
+    public void initialize(final @Nonnull Supplier<Space> constructor) {
+        checkNotNull(constructor, "constructor");
+        checkState(free.get() == null, "wheel already initialized");
+
+        final List<Consumer<Space>> freeUpdates = new LinkedList<>();
+        final List<Consumer<Space>> writingUpdates = new LinkedList<>();
+        final List<Consumer<Space>> readingUpdates = new LinkedList<>();
+
+        free.set(new Position<>(constructor.get(), freeUpdates, writingUpdates, readingUpdates));
+        mover = new MoverImpl(new Position<>(constructor.get(), writingUpdates, readingUpdates, freeUpdates));
+        reader = new ReaderImpl(new Position<>(constructor.get(), readingUpdates, writingUpdates, freeUpdates));
+    }
+
+    public @Nonnull Reader<Space> getReader() {
+        checkState(reader != null, "wheel must be initialized before position");
+        return reader;
+    }
+
+    public @Nonnull Mover<Space> getMover() {
+        checkState(mover != null, "wheel must be initialized before position");
+        return mover;
+    }
+
+    private class ReaderImpl implements Reader<Space> {
+        private Position<Space> position;
+
+        public ReaderImpl(final @Nonnull Position<Space> position) {
+            this.position = position;
+        }
+
+        @Override
+        public boolean read(final @Nonnull Consumer<Space> readTick) {
+            checkNotNull(readTick, "readTick");
+
+            position = free.getAndSet(position);
+            return position.read(readTick);
+        }
+    }
+
+    private class MoverImpl implements Mover<Space> {
+        private final Mover<Space> readingPhase = new Mover<Space>() {
+
+            @Override
+            public void read(final @Nonnull Consumer<Space> readTick) {
+                position.updateAndRead(readTick);
+                currentPhase = writingPhase;
+            }
+
+            @Override
+            public void move(final @Nonnull Consumer<Space> moveTick) {
+                throw new IllegalStateException("position must be read before position");
+            }
+        };
+
+        private final Mover<Space> writingPhase = new Mover<Space>() {
+            @Override
+            public void read(final @Nonnull Consumer<Space> readTick) {
+                throw new IllegalStateException("position already read");
+            }
+
+            @Override
+            public void move(final @Nonnull Consumer<Space> moveTick) {
+                position.write(moveTick);
+                position = free.getAndSet(position);
+                currentPhase = readingPhase;
+            }
+        };
+
+        private Mover<Space> currentPhase = readingPhase;
+        private Position<Space> position;
+
+        public MoverImpl(final @Nonnull Position<Space> position) {
+            this.position = position;
+        }
+
+        @Override
+        public void move(final @Nonnull Consumer<Space> moveTick) {
+            currentPhase.move(checkNotNull(moveTick, "writeTick"));
+        }
+
+        @Override
+        public void read(final @Nonnull Consumer<Space> readTick) {
+            currentPhase.read(checkNotNull(readTick, "readTick"));
+        }
+    }
+
+    private static class Position<Space> {
+        private final Space data;
+
+        // accessed only by mover
+        private List<Consumer<Space>> updates;
+        private List<Consumer<Space>> other0Updates;
+        private List<Consumer<Space>> other1Updates;
+
+        // accessed by reader and mover
+        private volatile ReadingStrategy<Space> readingStrategy = this::notReadyRead;
+
+        public Position(final @Nonnull Space initial,
+                        final @Nonnull List<Consumer<Space>> updates,
+                        final @Nonnull List<Consumer<Space>> other0Updates,
+                        final @Nonnull List<Consumer<Space>> other1Updates) {
+            this.data = initial;
+            this.updates = updates;
+            this.other0Updates = other0Updates;
+            this.other1Updates = other1Updates;
+        }
+
+        public boolean read(final @Nonnull Consumer<Space> readTick) {
+            return readingStrategy.read(readTick);
+        }
+
+        public void updateAndRead(final @Nonnull Consumer<Space> readTick) {
+            updates.forEach(tick -> tick.accept(data));
+            updates.clear();
+            readTick.accept(data);
+        }
+
+        public void write(final @Nonnull Consumer<Space> writeTick) {
+            writeTick.accept(data);
+
+            other0Updates.add(writeTick);
+            other1Updates.add(writeTick);
+
+            readingStrategy = this::readyRead;
+        }
+
+        @SuppressWarnings("unused")
+        private boolean notReadyRead(final @Nonnull Consumer<Space> readTick) {
+            return false;
+        }
+
+        private boolean readyRead(final @Nonnull Consumer<Space> readTick) {
+            readingStrategy = this::notReadyRead;
+            readTick.accept(data);
+            return true;
+        }
+    }
+
+    private interface ReadingStrategy<Space> {
+        boolean read(@Nonnull Consumer<Space> readTick);
+    }
+}
---
+++ src/test/groovy/almostsynced/SuperPositionSpec.groovy
@@ -0,0 +1,260 @@
+package almostsynced
+
+import spock.lang.Specification
+
+/**
+ * @author Maciej Chałapuk &lt;maciej@chalapuk.pl&gt;
+ */
+class SuperPositionSpec extends Specification {
+
+    class TestState {
+        String data = "initial"
+    }
+
+    def "throws when initializing with null constructor" () {
+        given:
+        def testedPosition = new SuperPosition<TestState>()
+
+        when:
+        testedPosition.initialize null
+
+        then:
+        def e = thrown(NullPointerException)
+        e.message == "constructor"
+    }
+
+    def "throws when reading not initialized" () {
+        given:
+        def testedPosition = new SuperPosition<TestState>()
+
+        when:
+        testedPosition.getReader()
+
+        then:
+        def e = thrown(IllegalStateException)
+        e.message == "wheel must be initialized before position"
+    }
+
+    def "throws when moving not initialized" () {
+        given:
+        def testedPosition = new SuperPosition<TestState>()
+
+        when:
+        testedPosition.getMover()
+
+        then:
+        def e = thrown(IllegalStateException)
+        e.message == "wheel must be initialized before position"
+    }
+
+    def "throws when initializing twice" () {
+        given:
+        def testedPosition = new SuperPosition<TestState>()
+
+        when:
+        testedPosition.initialize { new TestState() }
+        testedPosition.initialize { new TestState() }
+
+        then:
+        def e = thrown(IllegalStateException)
+        e.message == "wheel already initialized"
+    }
+
+    def "throws when calling Mover.move before Mover.read" () {
+        given:
+        def testedPosition = new SuperPosition<TestState>()
+
+        when:
+        testedPosition.initialize { new TestState() }
+        testedPosition.getMover().move {}
+
+        then:
+        def e = thrown(IllegalStateException)
+        e.message == "position must be read before position"
+    }
+
+    def "initializes first copy of state" () {
+        given:
+        def testedPosition = new SuperPosition<TestState>()
+
+        when:
+        testedPosition.initialize { new TestState() }
+        def mover = testedPosition.getMover()
+
+        TestState state0 = null
+        mover.read({ state -> state0 = state })
+
+        then:
+        state0.data == "initial"
+    }
+
+    def "initializes second copy of state" () {
+        given:
+        def testedPosition = new SuperPosition<TestState>()
+
+        when:
+        testedPosition.initialize { new TestState() }
+        def mover = testedPosition.getMover()
+
+        mover.read({ state -> })
+        mover.move({ state -> })
+
+        TestState state1 = null
+        mover.read({ state -> state1 = state })
+
+        then:
+        state1.data == "initial"
+    }
+
+    def "initializes third copy of state" () {
+        given:
+        def testedPosition = new SuperPosition<TestState>()
+
+        when:
+        testedPosition.initialize { new TestState() }
+        def mover = testedPosition.getMover()
+
+        mover.read({ state -> })
+        mover.move({ state -> })
+        mover.read({ state -> })
+        mover.move({ state -> })
+
+        TestState state2 = null
+        mover.read({ state -> state2 = state })
+
+        then:
+        state2.data == "initial"
+    }
+
+    def "mover reads state modified in previous tick" () {
+        given:
+        def testedPosition = new SuperPosition<TestState>()
+
+        when:
+        testedPosition.initialize { new TestState() }
+        def mover = testedPosition.getMover()
+
+        mover.read({ state -> })
+        mover.move({ state -> state.data = "modified" })
+
+        TestState state2 = null
+        mover.read({ state -> state2 = state })
+
+        then:
+        state2.data == "modified"
+    }
+
+    def "mover reads state modified two ticks before" () {
+        given:
+        def testedPosition = new SuperPosition<TestState>()
+
+        when:
+        testedPosition.initialize { new TestState() }
+        def mover = testedPosition.getMover()
+
+        mover.read({ state -> })
+        mover.move({ state -> state.data = "modified" })
+        mover.read({ state -> })
+        mover.move({ state -> })
+
+        TestState state2 = null
+        mover.read({ state -> state2 = state })
+
+        then:
+        state2.data == "modified"
+    }
+
+    def "reader not ready after initialization" () {
+        given:
+        def testedPosition = new SuperPosition<TestState>()
+
+        when:
+        testedPosition.initialize { new TestState() }
+        def reader = testedPosition.getReader()
+
+        def ready = reader.read({ state -> })
+
+        then:
+        !ready
+    }
+
+    def "reader ready after first copy modified" () {
+        given:
+        def testedPosition = new SuperPosition<TestState>()
+
+        when:
+        testedPosition.initialize { new TestState() }
+        def mover = testedPosition.getMover()
+        def reader = testedPosition.getReader()
+
+        mover.read({ state -> })
+        mover.move({ state -> })
+        def ready = reader.read({ state -> })
+
+        then:
+        ready
+    }
+
+    def "reader reads state modified by mover" () {
+        given:
+        def testedPosition = new SuperPosition<TestState>()
+
+        when:
+        testedPosition.initialize { new TestState() }
+        def mover = testedPosition.getMover()
+        def reader = testedPosition.getReader()
+
+        mover.read { state -> }
+        mover.move { state -> state.data = "modified" }
+
+        TestState state2 = null
+        reader.read { state -> state2 = state }
+
+        then:
+        state2.data == "modified"
+    }
+
+    def "reader not ready after reading first state modified by mover" () {
+        given:
+        def testedPosition = new SuperPosition<TestState>()
+
+        when:
+        testedPosition.initialize { new TestState() }
+        def mover = testedPosition.getMover()
+        def reader = testedPosition.getReader()
+
+        mover.read { state -> }
+        mover.move { state ->  }
+
+        reader.read { state -> }
+        def ready = reader.read { state -> }
+
+        then:
+        !ready
+    }
+
+    def "reader not ready after reading last state modified by mover" () {
+        given:
+        def testedPosition = new SuperPosition<TestState>()
+
+        when:
+        testedPosition.initialize { new TestState() }
+        def mover = testedPosition.getMover()
+        def reader = testedPosition.getReader()
+
+        mover.read { state -> }
+        mover.move { state ->  }
+        mover.read { state -> }
+        mover.move { state ->  }
+        mover.read { state -> }
+        mover.move { state ->  }
+        mover.read { state -> }
+        mover.move { state ->  }
+
+        reader.read { state -> }
+        def ready = reader.read { state -> }
+
+        then:
+        !ready
+    }
+}
